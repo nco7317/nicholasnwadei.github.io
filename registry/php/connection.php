00e46a247dc8459cd880a633c0b414f80df8279b
nco7317 <nicholasnwadei@gmail.com>
2020-11-04 21:46:13 -0600

    remove unnecessary folders

--- registry/php/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "micwydydwiiyp";
-    $dbname = "gifts";
-$conn = mysqli_connect($host, $dbUsername, $dbPassword, $dbname);
-
-?>
